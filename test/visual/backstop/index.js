2bb824f758c9d27edbb96a624be8f441c3023dcd
Boris Kaiser <mail@boriskaiser.com>
2017-04-04 21:24:04 +0200

    Change misMatchThreshold to 0

--- test/visual/backstop/index.js
+++ test/visual/backstop/index.js
@@ -65,7 +65,7 @@ module.exports = {
                 '#test_content_only .test-wrapper',
             ],
             delay: 0,
-            misMatchThreshold: 0.1,
+            misMatchThreshold: 0,
             onReadyScript: 'on-before.js',
         },
     ],
